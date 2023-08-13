13e5cf6906cad24a8da469fbd7a61f27600e8d8e
Juferoga <juancholoco2000.61@gmail.com>
2023-08-13 12:01:54 -0500

    Espacios de estado ranas

---
+++ README.md
@@ -0,0 +1,25 @@
+# 👻  **Inteligencia Artificial II**
+
+<img src="https://www.udistrital.edu.co/themes/custom/versh/images/default/preloader.png" width="192px" height="192px" align="right"/>
+
+[![Juan Felipe Rodriguez Galindo](https://img.shields.io/badge/Juferoga-github-br?style=flat-square)][1]
+[![Dayana Acuña](https://img.shields.io/badge/BrayanYate-github-br?style=flat-square)][6]
+[![License](https://img.shields.io/badge/License-MIT-blue?style=flat-square)][2]
+
+Repositorio para almacenar las tareas, talleres, ejercicios, entre otros que se desarrollen a lo largo de la materia Inteligencia Artificial II.
+
+## Contenido
+
+1. Ejercicios  → Desarrollo de ejercicios de la materia.
+2. Docs  → Documentación del proyecto alojada en [github][5].
+3. ETC   → Cosas random a organizar.
+
+## Contributors
+
+- [Juan Felipe Rodríguez Galindo - Código 20181020158][1]
+- [Dayana Acuña. 20192020151][6]
+
+ [1]:https://gitlab.com/Juferoga
+ [2]:https://github.com/Juferoga/patrones/blob/main/LICENSE
+ [5]:https://github.com/Juferoga/patrones
+ [6]:https://gitlab.com/BrayanYate
---
+++ ejercicios/corte-1/ranas.py
@@ -0,0 +1,33 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_agraph import graphviz_layout
+
+def valid_moves(state):
+    moves = []
+    for i, frog in enumerate(state):
+        if frog == 'G' and i < len(state) - 1 and state[i+1] == 'V':
+            moves.append(state[:i] + 'V' + 'G' + state[i+2:])
+        if frog == 'G' and i < len(state) - 2 and state[i+1] != 'V' and state[i+2] == 'V':
+            moves.append(state[:i] + 'V' + state[i+1] + 'G' + state[i+3:])
+        if frog == 'M' and i > 0 and state[i-1] == 'V':
+            moves.append(state[:i-1] + 'M' + 'V' + state[i+1:])
+        if frog == 'M' and i > 1 and state[i-1] != 'V' and state[i-2] == 'V':
+            moves.append(state[:i-2] + 'M' + state[i-1] + 'V' + state[i+1:])
+    return moves
+
+def build_tree(state, graph, depth=0, max_depth=5):
+    if depth > max_depth or state == 'MMMVVGGG':
+        return
+    moves = valid_moves(state)
+    for move in moves:
+        if move not in graph:
+            build_tree(move, graph, depth+1, max_depth)
+        graph.add_edge(state, move)
+
+state = "GGGVVMMM"
+graph = nx.DiGraph()
+build_tree(state, graph)
+
+pos = graphviz_layout(graph, prog="dot")
+nx.draw(graph, pos, with_labels=True, node_size=500, node_color="lightblue", arrowsize=10)
+plt.show()
